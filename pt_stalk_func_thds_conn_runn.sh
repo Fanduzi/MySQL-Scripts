34a66b6222e547e205408b6ab1a1db7f7e3b6ba2
Fan() <18501341937@163.com>
2018-04-04 23:45:28 -0500

    pt-stalk监测脚本
    threads_connecting > x 或 threads_running > y 时开始收集

--- pt_stalk_func_thds_conn_runn.sh
+++ pt_stalk_func_thds_conn_runn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 trg_plugin() {
-mysqladmin -uroot -p"ac0c4950a38b" -S /data/mysqldata/3306/mysql.sock extended-status |\
+mysqladmin -uroot -p"fanboshi" -S /data/mysqldata/3308/mysql.sock extended-status |\
 awk -F"|" \
 "BEGIN{ count=0; }"\
 '{ if ($2 ~ /Threads_connected/){Threads_connected=$3;}\
@@ -8,9 +8,6 @@ else if ($2 ~ /Threads_running/){Threads_running=$3;}\
 else if ($2 ~ /Uptime / && count >= 0){\
   print Threads_connected,Threads_running;\
 }}' | awk 'BEGIN{counter=0} \
-{ if ($1 > 1000 || $2 > 128 ) counter++ ; } END {print counter}'
+{ if ($1 > 1000 || $2 > 32 ) counter++ ; } END {print counter}'
 }
 trg_plugin
-
-#使用
-#/usr/bin/pt-stalk --collect-tcpdump --function /data/scripts/bin/pt_stalk_func_thds_conn_runn.sh --threshold 0 --daemonize --cycles 3 --user=root --ask-pass --socket=/data/mysqldata/3306/mysql.sock
